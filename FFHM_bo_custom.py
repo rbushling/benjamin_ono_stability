310b9aafec97c1da60c0a3721943e021bca37346
rbushling <regb1294@gmail.com>
2019-01-07 20:28:15 -0800

    Some debugging
    Still haven't quite gotten the spectra to display properly.

--- FFHM_bo_custom.py
+++ FFHM_bo_custom.py
@@ -20,14 +20,12 @@ def FFHM(L, D, f_hats, extras=False):
         for col in range(0, N):
             factor = 1j*(mu + 2*np.pi*(col-mid)/L)
             matrix[:,col] *= factor ** exponent
+            if exponent == 2:
+                matrix[:,col] *= -1j*np.sign(np.imag(factor))
         return matrix
 
     for mu in frange(-np.pi/L, np.pi/L, 2*np.pi/(L*D)):
         f_hats_copy = np.array(f_hats, dtype=np.complex_)
-        f_hats_copy[0,mid] = 1j * np.sign(mu)          # np.sign([mu + 2*np.pi*j/L for j in range(0,N)])
-        if np.abs(mu) < np.pi/(2*L*D):
-            f_hats_copy[0,mid] = 0
-
         matrix_dict = {}
         for i in range(0, M):
             mat = f_hats_copy[i,mid] * np.eye(N,dtype=np.complex_)
@@ -36,6 +34,7 @@ def FFHM(L, D, f_hats, extras=False):
                        + f_hats_copy[i,mid-j] * np.eye(N,k=-j,dtype=np.complex_)
             matrix_dict.update({M-(i+1): mat})
         matrix_dict = {key: augment_cols(value, key) for key, value in matrix_dict.items()}
+
         new_eigs = np.linalg.eigvals(sum(matrix_dict.values()))
         eigs = np.append(eigs, new_eigs)
         if extras:
